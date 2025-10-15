9501e6b122c15062440561754a52a311d3a69ae6
Krishnaraj Chidhanandh (XC-AS/EPY4) <Chidhanandh.Krishnaraj@bosch.com>
2025-10-15 09:54:08 +0200

    fixes for the white texts

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/my-portfolio/', // must match the repo name EXACTLY, with trailing slash
+  base: '/my-portfolio/',   // must match repo name (with trailing slash)
 })
